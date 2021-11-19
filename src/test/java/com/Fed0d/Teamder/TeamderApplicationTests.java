1955360abbef0f19de8c34744fe6b1babb796d40
Fed0d <loldffgf@yandex.ru>
2021-11-19 19:05:32 +0300

    Start

---
+++ src/test/java/com/Fed0d/Teamder/TeamderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fed0d.Teamder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
