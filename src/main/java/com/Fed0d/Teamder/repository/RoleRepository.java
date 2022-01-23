e43f5534297c84591e5e036bb33b45cab1db42dc
Fed0d <loldffgf@yandex.ru>
2022-01-23 19:01:31 +0300

    Login and registration

---
+++ src/main/java/com/Fed0d/Teamder/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.Fed0d.Teamder.repository;
+
+
+import com.Fed0d.Teamder.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
